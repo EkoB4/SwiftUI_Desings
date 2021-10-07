a8222061ec4d6ccf6f4f2ebce1faba8b8196616b
Ekin Bacik <ekinbacik@gmail.com>
2021-10-08 00:10:44 +0300

    new code added

--- Elements/Screens.swift
+++ Elements/Screens.swift
@@ -9,34 +9,43 @@ import SwiftUI
 struct startScreen : View {
     var body: some View{
         NavigationView{
+            GeometryReader{geo in
+               Spacer()
+        VStack{
+            MapView().frame(width:600 ,height:600).ignoresSafeArea()
             HStack{
-                Image("startScreenImage").ignoresSafeArea()
-                    .overlay(Text("Find Your Safe Way").font(.system(size: 60)).bold().foregroundColor(.white).shadow(radius: 100).frame(width: 300, height: 400).offset(y:80)).padding()
-                    .overlay(buttonUI().offset(y:300))
+                MainScreenElements().frame(width: geo.size.width*0.30, height: geo.size.height*0)
+                Text("City Library")
+                    .bold()
+                    .frame(width: 140)
+                    .font(.title2)
+                    .offset(x: -265, y: -150)
+                    .foregroundColor(.white)
+                Text("List Available")
+                    .frame(width:140)
+                    .font(.subheadline)
+                    .offset(x: -410, y: -60)
+                    .foregroundColor(.gray)
             }
+            NavigationLink(
+                destination: Text("Destination"),
+                label: {
+                    Text("CHANGE").font(.title3).foregroundColor(.orange).bold().offset(y:-180)
+                        .background(RoundedRectangle(cornerRadius: 30)
+                                        .fill(Color.white)
+                                        .frame(width: geo.size.width*0.3, height: geo.size.height/20).offset(y:-180)
+                                        )
+                })
         }
     }
+   }
 }
 
-struct buttonUI : View {
-    var body: some View{
-        NavigationLink(
-            destination: ContentView() ,
-            label: {
-                Text("Let's Start")
-                    .font(.title)
-                    .bold()
-                    .foregroundColor(.white)
-                    .background(RoundedRectangle(cornerRadius: 30)
-                                    .fill(Color("buttonColor"))
-                                    .frame(width: 300, height: 70))
-            })
-    }
-}
 struct Screen_Previews : PreviewProvider {
     static var previews: some View{
         startScreen()
         //MainScreenElements()
+        cityList()
     }
 }
 
@@ -47,7 +56,23 @@ struct MainScreenElements :View {
        RoundedRectangle(cornerRadius: 40)
         .frame(width: 430, height: 400).foregroundColor(Color("rectangleColor")).offset(x:222)
             RoundedRectangle(cornerRadius: 50).foregroundColor(.white)
-                .frame(width: 440, height: 360).ignoresSafeArea().offset(x:-220 ,y: 70)
+                .frame(width: 440, height: 360).ignoresSafeArea().offset(x:-220 ,y: 80)
     }
   }
+}
+}
+
+struct cityList : View {
+    var body: some View{
+        VStack{
+            ScrollView(.horizontal){
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 300, height: 160)
+                    .foregroundColor(.blue.opacity(0.2))
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 300, height: 160).offset(x:310 ,y: -167)
+                    .foregroundColor(.blue.opacity(0.2))
+            }
+        }
+    }
 }
--- uplabScreenDesing/ContentView.swift
+++ uplabScreenDesing/ContentView.swift
@@ -10,39 +10,32 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView{
-            GeometryReader{geo in
-               Spacer()
-        VStack{
-            MapView().frame(width:600 ,height:500).ignoresSafeArea()
             HStack{
-                MainScreenElements().padding(.bottom, 13).frame(width: geo.size.width*0.30, height: geo.size.height*0)
-                Text("City Library")
-                    .frame(width: 140)
-                    .font(.title2)
-                    .offset(x: -265, y: -160)
-                    .foregroundColor(.white)
-                Text("List Available")
-                    .frame(width:140)
-                    .font(.subheadline)
-                    .offset(x: -410, y: -60)
-                    .foregroundColor(.gray)
+                Image("startScreenImage").ignoresSafeArea()
+                    .overlay(Text("Find Your Safe Way").font(.system(size: 60)).bold().foregroundColor(.white).shadow(radius: 100).frame(width: 300, height: 400).offset(y:80)).padding()
+                    .overlay(buttonUI().offset(y:300))
             }
-            NavigationLink(
-                destination: Text("Destination"),
-                label: {
-                    Text("CHANGE").font(.title3).foregroundColor(.orange).bold().offset(y:-180)
-                        .background(RoundedRectangle(cornerRadius: 30)
-                                        .fill(Color.white)
-                                        .frame(width: geo.size.width*0.3, height: geo.size.height/15).offset(y:-180)
-                                        )
-                })
         }
-    }
-  }
 }
 }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct buttonUI : View {
+    var body: some View{
+        NavigationLink(
+            destination: startScreen() ,
+            label: {
+                Text("Let's Start")
+                    .font(.title)
+                    .bold()
+                    .foregroundColor(.white)
+                    .background(RoundedRectangle(cornerRadius: 30)
+                                    .fill(Color("buttonColor"))
+                                    .frame(width: 300, height: 70))
+            })
     }
 }
